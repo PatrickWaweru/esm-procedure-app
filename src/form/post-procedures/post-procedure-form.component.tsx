d979f8668951a61e9a016b556418e1ef65863a3e
Patrick Waweru <patrick.waweru@thepalladiumgroup.com>
2024-04-18 15:25:30 +0300

    Fixed issue with posting participants in POST Procedure Form

--- src/form/post-procedures/post-procedure-form.component.tsx
+++ src/form/post-procedures/post-procedure-form.component.tsx
@@ -46,17 +46,7 @@ const validationSchema = z.object({
   endDatetime: z.date({ required_error: "End datetime is required" }),
   outcome: z.string({ required_error: "Outcome is required" }),
   procedureReport: z.string({ required_error: "Procedure report is required" }),
-  participants: z.array(
-    z.object({
-      uuid: z.string(),
-      display: z.string(),
-      person: z.object({
-        uuid: z.string(),
-        display: z.string(),
-      }),
-    }),
-    { required_error: "Participants are required" }
-  ),
+  participants: z.string().optional(),
   complications: z.string().optional(),
 });
 
@@ -134,9 +124,10 @@ const PostProcedureForm: React.FC<PostProcedureFormProps> = ({
 
   const onSubmit = async (data: PostProcedureFormSchema) => {
     const participants = [];
+    console.warn("Selected: ", selectedProvider);
     if (selectedProvider) {
       const provider = {
-        provider: selectedProvider.concept.uuid,
+        provider: selectedProvider.uuid,
         encounterRole: encounterRole,
       };
       participants.push(provider);
@@ -353,7 +344,7 @@ const PostProcedureForm: React.FC<PostProcedureFormProps> = ({
                       <li
                         role="menuitem"
                         className={styles.condition}
-                        key={searchResult?.concept?.uuid}
+                        key={searchResult?.uuid}
                         onClick={() => handleProviderChange(searchResult)}
                       >
                         {searchResult.display}
